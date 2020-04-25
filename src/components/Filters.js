2636c6fd547c413286118e84a2f17e4478bb5b8e
osgood gunawan <ogunawan@ucsd.edu>
2020-04-24 21:23:26 -0400

    Done.

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,12 +1,12 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-class Filters extends React.Component {
-  render() {
-    return (
-      <div className="ui form">
+const Filters = ({onChangeType, onFindPetsClick}) => {
+  return (
+    <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select name="type" id="type" onChange={onChangeType}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -14,12 +14,41 @@ class Filters extends React.Component {
           </select>
         </div>
 
-        <div className="field">
-          <button className="ui secondary button">Find pets</button>
+        <div className="field" >
+          <button className="ui secondary button" onClick={onFindPetsClick}>Find pets</button>
         </div>
       </div>
-    )
-  }
+  )
 }
+
+// class Filters extends React.Component {
+//   render() {
+//     const {onChangeType,onFindPetsClick}= this.props
+//     return (
+//       <div className="ui form">
+//         <h3>Animal type</h3>
+//         <div className="field">
+//           <select name="type" id="type" onChange={onChangeType}>
+//             <option value="all">All</option>
+//             <option value="cat">Cats</option>
+//             <option value="dog">Dogs</option>
+//             <option value="micropig">Micropigs</option>
+//           </select>
+//         </div>
+
+//         <div className="field" >
+//           <button className="ui secondary button" onClick={onFindPetsClick}>Find pets</button>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+
+Filters.propTypes={
+  onChangeType: PropTypes.func.isRequired,
+  onFindPetsClick: PropTypes.func.isRequired
+}
+
+
 
 export default Filters
--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,29 +1,56 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 class Pet extends React.Component {
+
+  // handleClick = (e) => {
+  //   e.preventDefault()
+    
+  //   const {pet,onAdoptPet} = this.props
+  //   const {id} = pet
+
+  //   onAdoptPet(id)
+    
+
+  //   console.log(`this is click ${pet.id}`)
+  // }
+
   render() {
+    const {pet, onAdoptPet} = this.props
+    const {id, name, gender, type, age, weight, isAdopted} = pet
+
     return (
       <div className="card">
         <div className="content">
           <a className="header">
             {/*'♀' OR '♂' */}
-            PET NAME
+            {gender === "male" ? "♂ " : "♀ "}
+            {name}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+    <span className="date">{type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age:{age}</p>
+    <p>Weight: {weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          { isAdopted ? 
+          (<button className="ui disabled button"> Already adopted</button>) :
+        
+          (<button className="ui primary button" onClick={ e => onAdoptPet(id)} >Adopt pet</button>)
+          }
+
         </div>
       </div>
     )
   }
+}
+
+Pet.propTypes = {
+  pet: PropTypes.object.isRequired,
+  onAdoptPet: PropTypes.func.isRequired
 }
 
 export default Pet
